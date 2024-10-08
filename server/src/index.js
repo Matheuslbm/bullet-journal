0c45d3eab605c2e3e86d29292a9d2bce2dd4e551
Matheus lima <matheusslimabm@gmail.com>
2024-10-08 20:31:01 -0300

    tested it with the auth post route  auth
    I connected the prism and tested it with the auth post route to create an account and log in

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import authRoutes from './routes/auth.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 //Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes)
 
 // Rota simples para testar
 app.get('/', (req, res) => {
